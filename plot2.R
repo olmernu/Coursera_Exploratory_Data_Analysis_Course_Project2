3a0c3fceb6df372a98592357ed0cc5c743c3ef42
olmernu <olmernu@gmail.com>
2016-05-02 02:13:11 -0600

    R Script plot2

---
+++ plot2.R
@@ -0,0 +1,31 @@
+################################################################
+##
+## Exploratory Data Analysis
+## Course Project 2
+## Olmer Nunez-Sosa
+## 2016-04-30
+##
+################################################################
+
+################################################################
+# Data
+
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+################################################################
+#Question 2
+#Have total emissions from PM2.5 decreased in the Baltimore City, Maryland 
+#(fips == "24510") #from 1999 to 2008? Use the base plotting system to make a plot 
+#answering this question.
+
+subsetNEI  <- NEI[NEI$fips=="24510", ]
+aggregatedTotalByYear <- aggregate(Emissions ~ year, subsetNEI, sum)
+
+png('plot2.png')
+barplot(height=aggregatedTotalByYear$Emissions, 
+        names.arg=aggregatedTotalByYear$year, 
+        xlab="years", 
+        ylab=expression('total PM'[2.5]*' emission'),
+        main=expression('Total PM'[2.5]*' in the Baltimore City, MD emissions at various years'))
+dev.off()
