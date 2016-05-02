beb6f30d43d7761869fe015860f2563f272268f2
olmernu <olmernu@gmail.com>
2016-05-02 02:11:43 -0600

    R Script plot1

---
+++ plot1.R
@@ -0,0 +1,31 @@
+################################################################
+##
+## Exploratory Data Analysis
+## Course Project 2
+## Olmer Nunez-Sosa
+## 2016-04-30
+##
+################################################################
+
+################################################################
+# Data
+
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+################################################################
+#Question 1
+#Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
+#Using the base plotting system, make a plot showing the total PM2.5 emission from all 
+#sources for each of the years 1999, 2002, 2005, and 2008.
+
+table(NEI$year)
+aggregatedTotalByYear <- aggregate(Emissions ~ year, NEI, sum)
+
+png('plot1.png')
+barplot(height=aggregatedTotalByYear$Emissions, 
+        names.arg=aggregatedTotalByYear$year, 
+        xlab="years", 
+        ylab=expression('total PM'[2.5]*' emission'),
+        main=expression('Total PM'[2.5]*' emissions at various years'))
+dev.off()
